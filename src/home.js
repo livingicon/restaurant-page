1ded8c6ab4a39b3dedc6b8ce8303940d1f689aad
Josh Hancock <livingicon@gmail.com>
2022-06-06 15:24:23 -0400

    Remove DOM elements from index.html and add homePage module function to add them with JS in the DOM

--- src/home.js
+++ src/home.js
@@ -1,3 +1,19 @@
-// function homePage() {
+const homePage = (function() { //module
+  
+  const content = document.getElementById('content');
 
-// }
+})();
+
+//1. make module function that appends elements
+//2. export to index.js
+
+/* <div id="headline">
+<h1>Ryuzashi Ramen</h1>
+</div>
+<div id="copy">
+<p>A midwestern take on authentic shoyu style ramen. Established 2020.</p>
+</div>
+<img src="./images/david-hunter-xhOkqfXYls8-unsplash.jpg" alt="ramen">
+<p>Photo by <a href="https://unsplash.com/@fla5h904?utm_source=unsplash&utm
+_medium=referral&utm_content=creditCopyText" target="_blank">David Hunter
+</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a></p> */
