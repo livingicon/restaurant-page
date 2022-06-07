f6d80d9619573e92ca8671b22c89d04ffe0d5aee
Josh Hancock <livingicon@gmail.com>
2022-06-07 18:09:55 -0400

    Add navigation buttons

--- src/home.js
+++ src/home.js
@@ -3,28 +3,43 @@ const LoadPage = (function() {
   const homePage = function() {
     const content = document.getElementById('content');
     const header = document.createElement('div');
+    const restaurantName = document.createElement('h1');
+    const navigation = document.createElement('nav');
+    const homeBtn = document.createElement('homeBtn');
+    const menuBtn = document.createElement('menuBtn');
+    const contactBtn = document.createElement('contactBtn');
     const main = document.createElement('div');
     const home = document.createElement('div');
-    const restaurantName = document.createElement('h1');
     const restaurantAbout = document.createElement('p');
-    const established = document.createElement('p');
     const ramen = document.createElement('img');
+    const established = document.createElement('p');
 
     header.setAttribute('id', 'header');
+    restaurantName.textContent = "Ryuzashi Ramen";
+    navigation.classList.add('nav-btns');
+    homeBtn.setAttribute('id', 'homeBtn');
+    menuBtn.setAttribute('id', 'menuBtn');
+    contactBtn.setAttribute('id', 'contactBtn');
+    homeBtn.textContent = "Home";
+    menuBtn.textContent = "Menu";
+    contactBtn.textContent = "Contact";
     main.setAttribute('id', 'main-content');
     home.setAttribute('id', 'home');
+    restaurantAbout.textContent = 
+    "A midwestern take on authentic shoyu style ramen";
     ramen.setAttribute('id', 'homeImg');
     ramen.setAttribute('src', './images/masahiro-miyagi-woF6s-esrds-unsplash.jpg')
     ramen.setAttribute('alt', 'ramen');
-    restaurantName.textContent = "Ryuzashi Ramen";
-    restaurantAbout.textContent = 
-    "A midwestern take on authentic shoyu style ramen";
     established.textContent = "Established 2020";
 
     content.appendChild(header);
+    header.appendChild(restaurantName);
+    header.appendChild(navigation);
+    navigation.appendChild(homeBtn);
+    navigation.appendChild(menuBtn);
+    navigation.appendChild(contactBtn);
     content.appendChild(main);
     main.appendChild(home);
-    header.appendChild(restaurantName);
     home.appendChild(restaurantAbout);
     home.appendChild(ramen);
     home.appendChild(established);
@@ -35,9 +50,4 @@ const LoadPage = (function() {
 
 export default LoadPage;
 
-
-{/*
-<p>Photo by <a href="https://unsplash.com/@fla5h904?utm_source=unsplash&utm
-_medium=referral&utm_content=creditCopyText" target="_blank">David Hunter
-</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a></p> 
-*/}
+// add menu and style menu
