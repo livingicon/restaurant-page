128edaf0e7fe1dde6aba8dfd5996632d7333cb43
Josh Hancock <livingicon@gmail.com>
2022-06-07 13:45:43 -0400

    Add initial styling and additional divs and ids

--- src/home.js
+++ src/home.js
@@ -2,11 +2,31 @@ const LoadPage = (function() {
 
   const homePage = function() {
     const content = document.getElementById('content');
-
+    const header = document.createElement('div');
+    const main = document.createElement('div');
+    const home = document.createElement('div');
     const restaurantName = document.createElement('h1');
-    restaurantName.textContent = "Ryuzashi Ramen";
+    const restaurantAbout = document.createElement('p');
+    const established = document.createElement('p');
+    const ramen = document.createElement('img');
 
-    content.appendChild(restaurantName);
+    header.setAttribute('id', 'header');
+    main.setAttribute('id', 'main-content');
+    home.setAttribute('id', 'home');
+    ramen.setAttribute('src', './images/dlkr-SWyMV4kPlDo-unsplash.jpg')
+    ramen.setAttribute('alt', 'ramen');
+    restaurantName.textContent = "Ryuzashi Ramen";
+    restaurantAbout.textContent = 
+    "A midwestern take on authentic shoyu style ramen";
+    established.textContent = "Established 2020";
+
+    content.appendChild(header);
+    content.appendChild(main);
+    main.appendChild(home);
+    header.appendChild(restaurantName);
+    home.appendChild(restaurantAbout);
+    home.appendChild(ramen);
+    home.appendChild(established);
   };
 
   return { homePage };
@@ -14,13 +34,9 @@ const LoadPage = (function() {
 
 export default LoadPage;
 
-/* <div id="headline">
-<h1>Ryuzashi Ramen</h1>
-</div>
-<div id="copy">
-<p>A midwestern take on authentic shoyu style ramen. Established 2020.</p>
-</div>
-<img src="./images/david-hunter-xhOkqfXYls8-unsplash.jpg" alt="ramen">
+
+{/*
 <p>Photo by <a href="https://unsplash.com/@fla5h904?utm_source=unsplash&utm
 _medium=referral&utm_content=creditCopyText" target="_blank">David Hunter
-</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a></p> */
+</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a></p> 
+*/}
