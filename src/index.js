2703901ef82b291cd9cd62a89e8e76ad2e1778ef
Josh Hancock <livingicon@gmail.com>
2022-06-09 11:12:51 -0400

    Add rest of menu content

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import contactPage from './contact';
 
 loadPage.website();
 homePage();
-// navigation();
+
 homeBtn.addEventListener('click', navigation);
 menuBtn.addEventListener('click', navigation);
 contactBtn.addEventListener('click', navigation);
@@ -14,10 +14,6 @@ function navigation(e) {
   const homeContent = document.getElementById('home-content');
   const menuContent = document.getElementById('menu-content');
   const contactContent = document.getElementById('contact-content')
-  
-  // homeBtn.addEventListener('click', navigation);
-  // menuBtn.addEventListener('click', navigation);
-  // contactBtn.addEventListener('click', navigation);
 
   if(e.target.id === "menuBtn" && 
   !document.getElementById('menu-content') === true && 
--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,88 @@
 import loadPage from './website';
 
 const menuPage = function() {
-
+  // Variables
   const menuContent = document.createElement('div');
   const menu = document.createElement('div');
+
   const dishOne = document.createElement('div');
   const dishOnePic = document.createElement('img');
   const dishOneName = document.createElement('h5');
   const dishOneDescription = document.createElement('p');
 
+  const dishTwo = document.createElement('div');
+  const dishTwoPic = document.createElement('img');
+  const dishTwoName = document.createElement('h5');
+  const dishTwoDescription = document.createElement('p');
+
+  const dishThree = document.createElement('div');
+  const dishThreePic = document.createElement('img');
+  const dishThreeName = document.createElement('h5');
+  const dishThreeDescription = document.createElement('p');
+
+  const dishFour = document.createElement('div');
+  const dishFourPic = document.createElement('img');
+  const dishFourName = document.createElement('h5');
+  const dishFourDescription = document.createElement('p');
+
+
+  //Content
   menuContent.setAttribute('id', 'menu-content');
   menu.setAttribute('id', 'menu-items');
+
   dishOne.setAttribute('id', 'dishOne');
   dishOnePic.classList.add('dishPics');
   dishOnePic.setAttribute('src', './images/dishOne.jpg')
   dishOnePic.setAttribute('alt', 'spicy miso ramen');
   dishOneName.textContent = "SPICY MISO RAMEN";
   dishOneDescription.textContent = "Citarasa sup miso pedas yang lezat.";
+
+  dishTwo.setAttribute('id', 'dishTwo');
+  dishTwoPic.classList.add('dishPics');
+  dishTwoPic.setAttribute('src', './images/dishTwo.jpeg')
+  dishTwoPic.setAttribute('alt', 'curry ramen');
+  dishTwoName.textContent = "CURRY RAMEN";
+  dishTwoDescription.textContent = "ramen dengan citarasa kuah kari.";
+
+  dishThree.setAttribute('id', 'dishThree');
+  dishThreePic.classList.add('dishPics');
+  dishThreePic.setAttribute('src', './images/dishThree.jpg')
+  dishThreePic.setAttribute('alt', 'abura soba');
+  dishThreeName.textContent = "ABURA SOBA";
+  dishThreeDescription.textContent = "Dikenal sebagai ramen tanpa sup.";
+
+  dishFour.setAttribute('id', 'dishFour');
+  dishFourPic.classList.add('dishPics');
+  dishFourPic.setAttribute('src', './images/dishFour.jpg')
+  dishFourPic.setAttribute('alt', 'yakiniku don');
+  dishFourName.textContent = "YAKINIKU DON";
+  dishFourDescription.textContent = 
+  "Irisan daging sapi lembut dengan saus yakiniku diatas nasi.";
+
   
+  //Append Content
   loadPage.content.appendChild(menuContent);
   menuContent.appendChild(menu);
+
   menu.appendChild(dishOne);
   dishOne.appendChild(dishOnePic);
   dishOne.appendChild(dishOneName);
   dishOne.appendChild(dishOneDescription);
+
+  menu.appendChild(dishTwo);
+  dishTwo.appendChild(dishTwoPic);
+  dishTwo.appendChild(dishTwoName);
+  dishTwo.appendChild(dishTwoDescription);
+
+  menu.appendChild(dishThree);
+  dishThree.appendChild(dishThreePic);
+  dishThree.appendChild(dishThreeName);
+  dishThree.appendChild(dishThreeDescription);
+
+  menu.appendChild(dishFour);
+  dishFour.appendChild(dishFourPic);
+  dishFour.appendChild(dishFourName);
+  dishFour.appendChild(dishFourDescription);
 };
 
 export default menuPage;
