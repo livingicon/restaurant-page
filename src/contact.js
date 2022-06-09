79e4a6b7dc27fefdb5b5cee37dcf04e8d72ff35a
Josh Hancock <livingicon@gmail.com>
2022-06-09 14:36:40 -0400

    Add contact content

--- src/contact.js
+++ src/contact.js
@@ -3,13 +3,35 @@ import loadPage from './website';
 const contactPage = function() {
 
   const contactContent = document.createElement('div');
+
+  const phoneNumberIcon = document.createElement('img');
   const phoneNumber = document.createElement('p');
 
+  const addressIcon = document.createElement('img');
+  const address = document.createElement('p');
+
+  const emailIcon = document.createElement('img');
+  const email = document.createElement('p');
+
+
   contactContent.setAttribute('id', 'contact-content');
-  phoneNumber.textContent = "(765)765-7654";
+
+  phoneNumberIcon.setAttribute('src', './images/phone-classic.png');
+  phoneNumber.textContent = "(765) 765-7654";
+
+  addressIcon.setAttribute('src', './images/map-marker-alert.png');
+  address.innerHTML = "765 Ramen Rd.<br />Kokomo, IN 46901";
+
+  emailIcon.setAttribute('src', './images/email-send.png');
+  email.textContent = "email@fake-email.com";
   
   loadPage.content.appendChild(contactContent);
+  contactContent.appendChild(phoneNumberIcon);
   contactContent.appendChild(phoneNumber);
+  contactContent.appendChild(addressIcon);
+  contactContent.appendChild(address);
+  contactContent.appendChild(emailIcon);
+  contactContent.appendChild(email);
 };
 
 export default contactPage;
